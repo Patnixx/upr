4f758aa0ffd08288aebce9cd9f9dc211d394750d
Patrik Nemcok <patrik.nemcok33@gmail.com>
2025-10-07 00:03:21 +0200

    zadanie_4 almost done, animation needs more movement

--- zadanie_3/terminal_na_lsd.c
+++ zadanie_3/terminal_na_lsd.c
@@ -49,28 +49,164 @@ void cross_line(int length)
     }
 }
 
-void stairs(int length)
+void stairs(int length, int color)
 {
-    for (int i = 0; i < 6; i++)
+    for (int i = 0; i < length; i++)
     {
-        move_to(i+1, i+1);
-        pick_color(i);
+        pick_color(color);
         draw_pixel();
+        move_right();
+        move_down();
 
     }
 }
 
-void flower(int width, int height)
+void flower(int width, int height, int col, int row)
 {
-    cross_line(width);
-    for(int i = 0; i < width; i++)
+    int mid_slash = width / 2;
+    int mid_mod = width % 2;
+    int mid = mid_slash + mid_mod;
+    int stem = height - width;
+    int length = 1;
+
+    while(length < (width + 1))
     {
-        while(i < width)
+        if(row != 1)
+            move_to(length+height+1, col);
+        else
+            move_to(length, col);
+        if(length == width || length == 1)
         {
-            move_left();
-            i++;
+            pick_color(0);
+            for(int i = 0; i < (width-2); i++)
+            {
+                move_right();
+                draw_pixel();
+            }
         }
+        else if(length == mid)
+        {
+            int l = mid_slash;
+            pick_color(0);
+            while(l > 0)
+            {
+                draw_pixel();
+                move_right();
+                l--;
+            }
+            pick_color(4);
+            draw_pixel();
+            int r = mid_slash;
+            pick_color(0);
+            while(r > 0)
+            {
+                move_right();
+                draw_pixel();
+                r--;
+            }
+        }
+        else
+        {
+
+            pick_color(0);
+            for (int i = 0; i < width; i++)
+            {
+                draw_pixel();
+                move_right();
+            }   
+        }
+        length++;
     }
+    
+    if(row == 2)
+        move_to(length + height + 1, col+mid_slash);
+    else
+        move_to(width + 1, col+mid_slash);
+    for(int i = 0; i < stem; i++)
+    {
+        pick_color(1);
+        draw_pixel();
+        move_down();
+    }
+}
+
+void flower_field(int rows, int cols)
+{
+    for(int i = 1; i <= rows; i++)
+    {
+        for(int j = 0; j < cols; j++)
+        {
+            flower(3, 6, j*4 + 2, i);
+        }
+        move_to(i*6 + 1, 2);
+    }
+}
+
+void goku()
+{
+    //hair
+    move_to(1, 2);
+    set_blue_color();
+    draw_pixel();
+    move_right();
+    draw_pixel();
+    move_to(2, 1);
+    draw_pixel();
+    move_right();
+
+    //face
+    set_yellow_color();
+    draw_pixel();
+
+    //neck
+    move_down();
+    set_red_color();
+    draw_pixel();
+    move_right();
+    set_yellow_color();
+    draw_pixel();
+
+    //body
+    set_red_color();
+    move_to(4, 1);
+    draw_pixel();
+    move_right();
+    set_yellow_color();
+    draw_pixel();
+    move_right();
+    draw_pixel();
+    move_right();
+    set_yellow_color();
+    draw_pixel();
+
+    //legs
+    move_to(5, 2);
+    set_red_color();
+    draw_pixel();
+    move_to(6,1);
+    draw_pixel();
+    move_right();
+    move_right();
+    draw_pixel();
+    move_down();
+
+    //kamehameha
+    char kamehameha[] = "KAMEHAMEHA!!!";
+    move_to(3, 5);
+    
+    for(int i = 0; i < 13; i++)
+    {
+        reset_color();
+        printf("%c", kamehameha[i]);
+        set_blue_color();
+        move_down();
+        move_left();
+        draw_pixel();
+        move_right();
+        move_up();
+        animate();
+    }
+    move_to(10, 1);
 }
 
 int main() {
@@ -78,8 +214,8 @@ int main() {
     clear_screen();
 
     //input na kelvin
-    int drawing = 0;
-    scanf("%d\n", &drawing);
+    int drawing = 4;
+    //scanf("%d\n", &drawing);
 
 
     switch (drawing)
@@ -88,14 +224,22 @@ int main() {
             cross_line(5);
             break;
         case 1:
-            stairs(5);
+            stairs(5, 1);
+            move_to(4, 7);
+            stairs(11, 2);
+            move_to(7, 12);
+            stairs(5, 4);
             break;
         case 2:
-            flower(5, 5);
+            flower(7, 12, 1, 1);
+            flower(5, 10, 9, 1);
+            flower(3, 8, 15, 1);
             break;
         case 3:
+            flower_field(4, 5);
             break;
         case 4:
+            goku();
             break;
     }
 
