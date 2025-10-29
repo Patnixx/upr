ff9f337f3992590aed76f84827a02c9f639c3c82
Patrik Nemcok <patrik.nemcok33@gmail.com>
2025-10-29 13:30:13 +0100

    fully functional zadanie_6, let's pray for good times

--- zadanie_6/dajtemirolupipinkyenjoyer.c
+++ zadanie_6/dajtemirolupipinkyenjoyer.c
@@ -2,7 +2,6 @@
 #include <string.h>
 #include <ctype.h>
 #include <stdbool.h>
-
 struct Statistic {
     int lowercase;
     int uppercase;
@@ -10,123 +9,194 @@ struct Statistic {
 };
 
 void readLines(char input[51]) {
-    fgets(input, 50, stdin);
+    if (fgets(input, 51, stdin) == NULL) //input riadkov 
+    { 
+        input[0] = '\0'; 
+        return; 
+    }
+
+    size_t n = strlen(input); //kelvin warning fix
+    if (n > 0 && input[n-1] == '\n') //vymaze posledny znak 
+    {
+        input[n-1] = '\0';
+    }
+    if (n > 1 && input[n-2] == '\r') //vymaze predposledny znak
+    {
+        input[n-2] = '\0';
+    }
 }
 
 void initBaseStats(struct Statistic *stat, char input[51]) {
     stat->lowercase = 0;
     stat->uppercase = 0;
     stat->spaces = 0;
-
-    for (int i = 0; i < strlen(input); i++) {
-        if (input[i] >= 'a' && input[i] <= 'z') { //ascii porovnavanie
+    for (size_t i = 0; i < strlen(input); i++) {
+        if (input[i] >= 'a' && input[i] <= 'z') //ascii porovnanie
+        {
             stat->lowercase++;
-        } else if (input[i] >= 'A' && input[i] <= 'Z') { //ascii porovnavanie
+        }
+        else if (input[i] >= 'A' && input[i] <= 'Z')  //ascii porovnanie
+        {
             stat->uppercase++;
-        } else if (input[i] == ' ') {
+        }
+        else if (input[i] == ' ') 
+        {
             stat->spaces++;
         }
     }
 }
 
-void normalize(char input[51], char normal[51], char letters[53], struct Statistic *normStat)
-{
+int gateToNarnia(const char *character) { //custom atoi()
+    int num = 0;
+    int i = 0;
+    while (character[i] != '\0' && character[i] >= '0' && character[i] <= '9') 
+    {
+        num = num * 10 + (character[i] - '0'); //mala nasobilka type shit
+        i++;
+    }
+    return num;
+}
+
+void normalize(char input[51], char normal[51], char letters[53], struct Statistic *normStat) {
     normStat->lowercase = 0;
     normStat->uppercase = 0;
     normStat->spaces = 0;
     strcpy(normal, input);
-    char *norm_line = strpbrk(normal, letters); //medzery na zaciatku
-    int len = strlen(norm_line);
-    while(len > 0 && norm_line[len - 1] == ' ') { //medzery na konci
-        norm_line[len - 1] = '\0';
-        len--;
+
+    char *norm_line = strpbrk(normal, letters); //preskoci medzery na zaciatku
+    if (!norm_line) //ak je len medzera
+    {
+        norm_line = normal;
     }
 
-    len = strlen(norm_line);
-    for (int i = 0; i < len; i++) {
-        if(norm_line[i] == ' ' && norm_line[i + 1] == ' ') { //len jedna medzera medzi slovami
-            memmove(&norm_line[i], &norm_line[i + 1], strlen(&norm_line[i + 1]) + 1);
+    size_t len = strlen(norm_line); //kelvin warning fix
+    while(len > 0 && norm_line[len - 1] == ' ') //medzery od konca
+    { 
+        norm_line[len - 1] = '\0'; 
+        len--; 
+    }
+
+    len = strlen(norm_line); //medzery medzi slovami
+    for (size_t i = 0; i < len; i++) 
+    {
+        if(norm_line[i] == ' ' && norm_line[i + 1] == ' ') 
+        {
+            memmove(&norm_line[i], &norm_line[i + 1], strlen(&norm_line[i + 1]) + 1); //posunie znaky z pamate aby bola medzi nimi jedna medzera
             i--;
         }
     }
 
-    //stats
-    for (int i = 0; i < strlen(norm_line); i++) {
-        if (norm_line[i] >= 'a' && norm_line[i] <= 'z') { //ascii porovnavanie
-            normStat->lowercase++;
-        } else if (norm_line[i] >= 'A' && norm_line[i] <= 'Z') { //ascii porovnavanie
-            normStat->uppercase++;
-        } else if (norm_line[i] == ' ') {
-            normStat->spaces++;
-        }
-    }
-
-    char merged[51];
+    char final[50] = ""; //final array kde budu pospajane slova
     char *word = strtok(norm_line, " ");
     bool empty = true;
-    while (word != NULL)
+    while (word != NULL) 
     {
-        //prezriet ci tam neni velke pismeno
-        int has_upper = 0;
-        for (int k = 0; word[k] != '\0'; k++) {
-            if (isupper(word[k])) {
-                has_upper = 1;
-                break;
+        size_t word_len = strlen(word);
+        bool is_upper = false;
+
+        for (int i = 0; word[i] != '\0'; i++)  //prejde slovo ci tam je velke pismeno
+        { 
+            if (isupper(word[i]))  
+            {   is_upper = true; 
+                break; 
             }
         }
-        //ak je/neni tak upravit slovo
-        if (has_upper) {
-            word[0] = toupper(word[0]);
-            for (int k = 1; word[k]; k++)
-                word[k] = tolower(word[k]);
+
+        if(is_upper) //nastavi prve pismeno velke ak su dalsie pismena v slove 
+        { 
+            for(size_t k = 0; k < word_len; k++)
+            {
+                if(k == 0) 
+                {
+                    word[k] = toupper(word[k]);
+                }
+                else 
+                {
+                    word[k] = tolower(word[k]);
+                }
+            }
         } 
-        else {
-            for (int k = 0; word[k]; k++)
+
+        else //iba prve pismeno bude velke 
+        { 
+            for(size_t k = 0; k < word_len; k++) 
+            {
                 word[k] = toupper(word[k]);
             }
-
-
-        //odstranenie duplicitnych pismen v slove
-        int w = 1;
-        for (int k = 1; word[k]; k++) {
-            if (word[k] != word[k-1])
-                word[w++] = word[k];
         }
-        word[w] = '\0';
 
-        if (empty == false) 
+        //picovske duplicitne znaky v slove
+        int picus = 1;
+        for (int k = 1; word[k]; k++) 
         {
-            strcat(merged, " ");
+            if (word[k] != word[k-1]) 
+            {
+                word[picus++] = word[k];
+            }
         }
-        else
-        {
-            strcat(merged, word);
+        word[picus] = '\0';
+
+        if (!empty) strcat(final, " ");
+        strcat(final, word);
         empty = false;
-        }
 
-        word = strtok(NULL, " ");
+        word = strtok(NULL, " "); //uzavrie slovo a ide na dalsie
     }
-    printf("%s\n", merged);
-    
+
+    for (size_t i = 0; i < strlen(final); i++) 
+    {
+        if (final[i] >= 'a' && final[i] <= 'z') //ascii porovnanie
+            {
+                normStat->lowercase++;
+            }
+        else if (final[i] >= 'A' && final[i] <= 'Z') //ascii porovnanie
+            {
+                normStat->uppercase++;
+            }
+        else if (final[i] == ' ') 
+            {
+                normStat->spaces++;
+            }
+    }
+    printf("%s\n", final);
 }
 
 void printStats(const struct Statistic *stat, const struct Statistic *normalised) {
-
     printf("lowercase: %d -> %d\n", stat->lowercase, normalised->lowercase);
     printf("uppercase: %d -> %d\n", stat->uppercase, normalised->uppercase);
     printf("spaces: %d -> %d\n", stat->spaces, normalised->spaces);
 }
 
 int main() {
-    int numOfLines = 0;
+    //vars
     char input[51];
     char normal[51];
     char letters[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
     struct Statistic stat;
     struct Statistic normalised;
 
-    readLines(input);
-    initBaseStats(&stat, input);
-    printStats(&stat, &normalised);
-    normalize(input, normal, letters, &normalised);
+    char numOfLinesStr[16] = "";
+    if (fgets(numOfLinesStr, sizeof(numOfLinesStr), stdin) == NULL) //ziadny ciselny vstup (test1) 
+        {
+            return 0;
+        }
+
+    size_t lines = strlen(numOfLinesStr);
+    if (lines && numOfLinesStr[lines-1] == '\n') //odjebe zo vstupu \n 
+        {
+            numOfLinesStr[lines-1] = '\0';
+        }
+
+    int numOfLines = gateToNarnia(numOfLinesStr); //custom atoi()
+
+
+    for (int i = 0; i < numOfLines; i++)
+    {
+        readLines(input);
+        initBaseStats(&stat, input);
+        normalize(input, normal, letters, &normalised);
+        printStats(&stat, &normalised);
+        if (i != numOfLines - 1) printf("\n");
+    }
+    return 0;
 }
